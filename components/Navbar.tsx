7137d2ea21c2270fc795fd000ae4b16f6bfeb207
marshoepial <marshoepial@gmail.com>
2021-07-05 18:21:22 -0400

    initial commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const navbarStyles = StyleSheet.create({
+  navView: {
+    width: '100%',
+    height: 50,
+    backgroundColor: 'coral',
+    alignItems: 'center',
+  },
+  headerText: {
+    height: '100%',
+    fontSize: 25,
+    fontWeight: '600',
+    flex: 1,
+    color: 'white',
+    lineHeight: 50,
+    marginTop: 2,
+  },
+});
+
+export const Navbar = () => (
+  <View style={navbarStyles.navView}>
+    <Text style={navbarStyles.headerText}>Test Header</Text>
+  </View>
+);
