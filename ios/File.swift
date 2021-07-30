75b02c75dc6980ea2a0c06dbe10ebfca9cc4b6a8
marshoepial <marshoepial@gmail.com>
2021-07-29 20:41:58 -0400

    Bono now works on ios and can compile on apple ARM

---
+++ ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  bono
+//
+
+import Foundation
--- ios/bono/AppDelegate.m
+++ ios/bono/AppDelegate.m
@@ -27,8 +27,10 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+#if DEBUG
 #ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
+#endif
 #endif
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
